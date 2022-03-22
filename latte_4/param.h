a7c75f1032811c0143195f0a24ff7592cda36371
Raymond K. Zhao <raymond.zhao@monash.edu>
2022-03-22 19:00:21 +1100

    Use binary128 for Latte-4

--- latte_4/param.h
+++ latte_4/param.h
@@ -2,6 +2,7 @@
 #define _PARAM_H
 
 #include <stdint.h>
+#include <quadmath.h>
 
 #define PREC 88
 
@@ -11,9 +12,9 @@
 
 #define L 2
 
-static const char sigma_str[L + 1][PREC] = {"9583.7471944500068780694380", "713170.79131244259284534655", "65489528.122674807214409517"}; 
+static const __float128 sigma_l[L + 1] = {9583.7471944500068780694380Q, 713170.79131244259284534655Q, 65489528.122674807214409517Q}; 
 
-static const char norm_str[L][PREC] = {"376210269336.07783606747078", "3124915676658988.2977945632"};
+static const __float128 norm_l[L] = {376210269336.07783606747078Q, 3124915676658988.2977945632Q};
 
 static const uint64_t reduce_k_prec[L] = {512, 1536};
 #define FFT_PREC 1536
